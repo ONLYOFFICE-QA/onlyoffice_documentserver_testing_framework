251a6660493822e6fa485f453dfd34d15323f232
Pavel Lobashov <shockwavenn@gmail.com>
2018-09-28 14:59:22 +0300

    Add spreadsheet

--- lib/onlyoffice_documentserver_testing_framework/test_instance_docs.rb
+++ lib/onlyoffice_documentserver_testing_framework/test_instance_docs.rb
@@ -1,5 +1,6 @@
 require_relative 'test_instance_docs/common_editor/editor_windows'
 require_relative 'test_instance_docs/doc_editor'
+require_relative 'test_instance_docs/spreadsheet_editor'
 require_relative 'test_instance_docs/management'
 
 module OnlyofficeDocumentserverTestingFramework
@@ -13,6 +14,10 @@ module OnlyofficeDocumentserverTestingFramework
 
     def doc_editor
       @doc_editor ||= DocEditor.new(self)
+    end
+
+    def spreadsheet_editor
+      @spreadsheet_editor ||= SpreadsheetEditor.new(self)
     end
 
     def management
