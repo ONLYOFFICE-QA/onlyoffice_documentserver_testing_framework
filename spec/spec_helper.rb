092d1a2ab89594a1ffbe3a7f2c1431db6cd9ea01
Pavel Lobashov <shockwavenn@gmail.com>
2018-10-01 11:40:14 +0300

    Add simplecov support

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+if ENV['CI'] == 'true'
+  require 'simplecov'
+  SimpleCov.start
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
+require_relative '../../lib/onlyoffice_documentserver_testing_framework'
