66746035c833959f1e763416765dcea24fca973f
Pavel Lobashov <shockwavenn@gmail.com>
2019-10-03 12:04:36 +0300

    Add new languages to example page
    Since:
    https://github.com/ONLYOFFICE/document-server-integration/commit/d12897930c681d5749daee911904264f17f1cd9c

---
+++ spec/doc_test_funtions/doc_test_functions_languages_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe DocTestSiteFunctions do
+  it '#language_list_options is correct' do
+    instance = OnlyofficeDocumentserverTestingFramework::TestInstanceDocs.new
+    instance.webdriver.open('http://localhost')
+    instance.doc_service_welcome.go_to_example
+    puts instance.doc_test_functions.language_list_options
+    expect(instance.doc_test_functions.language_list_options).to eq(DocTestSiteFunctions::SUPPORTED_LANGUAGES)
+  end
+end
