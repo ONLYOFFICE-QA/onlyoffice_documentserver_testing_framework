9e306c25ced6b0bf8be1308d1d60d9ceede994aa
Pavel Lobashov <shockwavenn@gmail.com>
2019-10-08 18:37:44 +0300

    Fix more typo

--- spec/management_spec.rb
+++ spec/management_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe 'Management' do
   it 'correclty waiting to load of page with document' do
     instance = OnlyofficeDocumentserverTestingFramework::TestInstanceDocs.new
-    instance.webdriver.open('https://localhost')
+    instance.webdriver.open('http://localhost')
     instance.doc_service_welcome.go_to_example
     instance.doc_test_functions.open_sample_document
     expect(instance.management.wait_for_operation_with_round_status_canvas).to be_truthy
