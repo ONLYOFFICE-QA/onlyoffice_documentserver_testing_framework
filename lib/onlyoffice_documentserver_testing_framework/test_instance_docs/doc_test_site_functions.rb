0d94fb6d4bac5ffe0aa9eb12efc22ea74724ca4f
Pavel Lobashov <shockwavenn@gmail.com>
2020-02-18 16:08:12 +0300

    Fix more

--- lib/onlyoffice_documentserver_testing_framework/test_instance_docs/doc_test_site_functions.rb
+++ lib/onlyoffice_documentserver_testing_framework/test_instance_docs/doc_test_site_functions.rb
@@ -49,7 +49,6 @@ class DocTestSiteFunctions
     @instance.webdriver.wait_until do
       doc_test_site?
     end
-    sleep(1) # TODO: remove after update to Chromedriver 80
   end
 
   # reload the page
@@ -62,6 +61,7 @@ class DocTestSiteFunctions
   # Check if current site is DocTestSite
   # @return [true, false] result of checking
   def doc_test_site?
+    sleep(1) # TODO: remove after update to Chromedriver 80
     result = @instance.selenium.element_present?(@xpath_begin_view)
     OnlyofficeLoggerHelper.log("Current server is a doc_test_site: #{result}")
     result
