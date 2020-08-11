ccc35ea449b1118e60e724c515d8dfe34276d97a
Pavel Lobashov <shockwavenn@gmail.com>
2020-08-11 11:56:47 +0300

    Fix forgotten version change

--- lib/onlyoffice_documentserver_testing_framework/version.rb
+++ lib/onlyoffice_documentserver_testing_framework/version.rb
@@ -4,6 +4,6 @@ module OnlyofficeDocumentserverTestingFramework
   # Module to hold version of Gem
   module Version
     # [String] version of gem
-    STRING = '0.1.0'
+    STRING = '0.1.1'
   end
 end
