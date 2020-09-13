48339874ae95baed0786a1db165b334d1905ac49
Pavel Lobashov <shockwavenn@gmail.com>
2020-09-13 22:09:41 +0300

    Release v0.2.0

--- lib/onlyoffice_documentserver_testing_framework/version.rb
+++ lib/onlyoffice_documentserver_testing_framework/version.rb
@@ -4,6 +4,6 @@ module OnlyofficeDocumentserverTestingFramework
   # Module to hold version of Gem
   module Version
     # [String] version of gem
-    STRING = '0.1.1'
+    STRING = '0.2.0'
   end
 end
