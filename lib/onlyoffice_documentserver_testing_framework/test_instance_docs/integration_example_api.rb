b38df6b801adb27a307e06f312af505df61a3fc9
Pavel Lobashov <shockwavenn@gmail.com>
2021-02-19 12:26:33 +0300

    Refactoring

--- lib/onlyoffice_documentserver_testing_framework/test_instance_docs/integration_example_api.rb
+++ lib/onlyoffice_documentserver_testing_framework/test_instance_docs/integration_example_api.rb
@@ -13,7 +13,10 @@ module OnlyofficeDocumentserverTestingFramework
 
     # @return [Array<Hash>] list of file on example
     def files
-      http_get("#{@api_endpoint}/files/")
+      url = "#{@api_endpoint}/files/"
+      request = Net::HTTP::Get.new(url)
+      response = http_from_url(url).request(request)
+      JSON.parse(response.read_body)
     end
 
     # Upload file by it's path
@@ -59,12 +62,6 @@ module OnlyofficeDocumentserverTestingFramework
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true if uri.scheme == 'https'
       http
-    end
-
-    def http_get(url)
-      request = Net::HTTP::Get.new(url)
-      response = http_from_url(url).request(request)
-      JSON.parse(response.read_body)
     end
   end
 end
