f82c43e1cb4e56d353ff38ad607e24a95179e1c8
Pavel Lobashov <ShockwaveNN@gmail.com>
2021-04-15 10:37:11 +0300

    New method to get doc name `TitleRow#document_name` (#200)

--- lib/onlyoffice_documentserver_testing_framework/test_instance_docs/doc_editor/doc_editor_top_toolbar.rb
+++ lib/onlyoffice_documentserver_testing_framework/test_instance_docs/doc_editor/doc_editor_top_toolbar.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 require_relative 'doc_editor_top_toolbar/top_toolbar_users'
+require_relative 'doc_editor_top_toolbar/title_row'
 
 module OnlyofficeDocumentserverTestingFramework
   # Top Toolbar of Doc Editor
   class DocEditorTopToolbar
     def initialize(instance)
       @instance = instance
+    end
+
+    # @return [TitleRow] class for methods in title row
+    def title_row
+      @title_row ||= TitleRow.new(@instance)
     end
 
     # @return [TopToolbarUsers] icon for top toolbar users
