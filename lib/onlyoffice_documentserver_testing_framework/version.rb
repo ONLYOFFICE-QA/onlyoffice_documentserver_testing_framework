99d5c749f3b0af36fd70418a14b0b43af6a2ecb2
Pavel Lobashov <shockwavenn@gmail.com>
2021-10-14 11:29:34 +0300

    Release v0.6.5
    Fix order `embedded` mode in test example

--- lib/onlyoffice_documentserver_testing_framework/version.rb
+++ lib/onlyoffice_documentserver_testing_framework/version.rb
@@ -4,6 +4,6 @@ module OnlyofficeDocumentserverTestingFramework
   # Module to hold version of Gem
   module Version
     # [String] version of gem
-    STRING = '0.6.4'
+    STRING = '0.6.5'
   end
 end
