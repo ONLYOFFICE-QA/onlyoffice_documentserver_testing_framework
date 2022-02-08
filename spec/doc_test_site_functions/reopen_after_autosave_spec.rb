d34b49692bd5f6eeb89b83409908b120b4cf4f1a
Pavel Lobashov <shockwavenn@gmail.com>
2022-02-08 15:53:43 +0300

    Bring back

--- spec/doc_test_site_functions/reopen_after_autosave_spec.rb
+++ spec/doc_test_site_functions/reopen_after_autosave_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 describe DocTestSiteFunctions, '#reopen_after_autosave' do
   it 'reopen_after_autosave fails for document without any changes' do
-    instance = OnlyofficeDocumentserverTestingFramework::TestInstanceDocs.new(doc_server_base_url: 'https://kim.teamlab.info')
+    instance = OnlyofficeDocumentserverTestingFramework::TestInstanceDocs.new(doc_server_base_url: 'http://localhost')
     instance.go_to_base_url
     instance.doc_service_welcome.go_to_example
     instance.doc_test_functions.open_sample_document
