dab22ec9eef4e916b7f49d3f7f9033e494ccc641
alinasafonova <53475320+alinasafonova@users.noreply.github.com>
2023-02-03 15:19:29 +0300

    Actualize list of interface languages for test example in 7.3 (#928)
    * Actualize list of interface languages for test example in 7.3
    
    * Increase the limit for class length

--- lib/onlyoffice_documentserver_testing_framework/test_instance_docs/doc_test_site_functions.rb
+++ lib/onlyoffice_documentserver_testing_framework/test_instance_docs/doc_test_site_functions.rb
@@ -225,8 +225,10 @@ class DocTestSiteFunctions
 
   # @param [DocumentServerVersion] version of server
   # @return [Array<String>] list of supported languages
-  def self.supported_languages(version = OnlyofficeDocumentserverTestingFramework::DocumentServerVersion.new(7, 2, 0))
-    file = if version >= OnlyofficeDocumentserverTestingFramework::DocumentServerVersion.new(7, 2)
+  def self.supported_languages(version = OnlyofficeDocumentserverTestingFramework::DocumentServerVersion.new(7, 3, 0))
+    file = if version >= OnlyofficeDocumentserverTestingFramework::DocumentServerVersion.new(7, 3)
+             'doc_test_site_languages_after_7_3.list'
+           elsif version >= OnlyofficeDocumentserverTestingFramework::DocumentServerVersion.new(7, 2)
              'doc_test_site_languages_after_7_2.list'
            elsif version >= OnlyofficeDocumentserverTestingFramework::DocumentServerVersion.new(7, 1)
              'doc_test_site_languages_after_7_1.list'
