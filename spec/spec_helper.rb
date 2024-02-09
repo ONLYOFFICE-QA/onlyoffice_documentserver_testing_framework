5c2b62d95e95f84ff2844dde2a79ce291919d64d
Vitaliy Serov <154601125+VitaliySerov@users.noreply.github.com>
2024-02-09 18:50:35 +0300

    Add support of `codecov` coverage reports (#1325)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,9 @@
 require 'simplecov'
 SimpleCov.start
 
+if ENV['CI']
+  require 'simplecov-cobertura'
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+end
+
 require_relative '../lib/onlyoffice_documentserver_testing_framework'
